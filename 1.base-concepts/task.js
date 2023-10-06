dddb7a2abbaff1d5b44084d9235f08f0183035b2
“betiwe” <betiwe@yandex.ru>
2023-10-06 14:02:35 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
+  let discriminant = b ** 2 - 4 * a * c;
   let arr = [];
-  
+
+  if (discriminant < 0) {
+    arr.push('Корней нет');
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / (100 * 12);
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (percent + percent / ((1 + percent) ** countMonths - 1));
+  let totalAmount = +(monthlyPayment * 12 - creditBody + amount).toFixed(2);
+
+  return totalAmount;
 }
