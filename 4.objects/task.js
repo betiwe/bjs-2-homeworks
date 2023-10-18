7379a466ccad1486ee3b7623930b06e832576f23
“betiwe” <betiwe@yandex.ru>
2023-10-18 21:25:39 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  Student.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if ('marks' in Student) {
+    this.marks = marks;
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if(!this.marks || this.marks.length === 0) {
+    return 0;
+  } else {
+    let count = 0;
+    for (let grades of this.marks) {
+      count += grades;
+    }
+    return count / this.marks.length;
+  }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+};
+
+
